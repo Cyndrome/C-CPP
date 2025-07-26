8ca80bf24ffb5dd4239ea3408c68811aa4b7f81b
Cyndrome <cyndrome.inmotion@gmail.com>
2025-07-26 22:12:03 +0600

    Uploaded permutation123

---
+++ permutation123.c
@@ -0,0 +1,28 @@
+/*
+    A C program to print all the possible permutations of the integers 
+    1, 2 and 3 using nested for loop.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    for (a = 1; a<=3; a++)
+    {
+        for (b=1; b<=3; b++)
+        {
+            if(b!=a)
+            {
+                for(c=1; c<=3; c++)
+                {
+                    if (c!=a && c!=b)
+                    {
+                        printf("%d, %d, %d\n", a, b, c);
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
