76908ac6aa7d9ffab76011cc4eb5b16a48df4a36
Sheikh Mashrafi Mohammad <mashrafi.mohammad@gmail.com>
2026-02-17 13:53:17 +0600

    Add files via upload
    Demonstrated passing and returning strings and arrays in user-defined functions [C implementation]

---
+++ function_str_arr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+char *reverseChar(char *str)
+{
+    int len = strlen(str);
+    char *s = malloc(len + 1);
+    for (int i = 0; i < len; i++)
+    {
+        s[i] = str[len - 1 - i];
+    }
+    s[len] = '\0';
+    return s;
+}
+*/
+int *reverse_arr(int *a, int size)
+{
+    int *r = malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
+        r[i] = a[size-1-i];
+    }
+    return r;
+}
+
+int main()
+{
+    /*
+    char in[1001];
+    scanf("%[^\n]", in);
+    char *rev = reverseChar(in);
+    printf("%s", rev);
+    free(rev);
+    return 0;
+    */
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int *rev = reverse_arr(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", rev[i]);
+    }
+    free (rev);
+    return 0;
+}
